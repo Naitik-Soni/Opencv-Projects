a79f475bcc571a0b1e3d1396f63d754bb6bcb6b0
Naitik Soni <soni.naitik>
2025-04-29 11:18:11 +0530

    Rename the file

--- Color-Manipulation/window_loader.py
+++ Color-Manipulation/window_loader.py
@@ -1,12 +1,10 @@
 import tkinter as tk
-import cv2
-import numpy as np
 
 app = tk.Tk()
 app.title("Color Manipulation")
 
 try:
-    icon = tk.PhotoImage(file=r"Assets\window-icon.png")  # Replace "my_icon.png" with the path to your image file
+    icon = tk.PhotoImage(file=r"Assets\window-icon.png")
     app.iconphoto(False, icon)
 except tk.TclError:
     print("Error: Could not load icon file.")
