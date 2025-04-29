1d4eda4e4152d32f78007539b1498bc725a93f46
Naitik Soni <naitiksoni1705@gmail.com>
2025-04-29 09:16:17 +0530

    Added starter code

---
+++ Color-Manipulation.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import cv2
+import numpy as np
+
+app = tk.Tk()
+app.title("Color Manipulation")
+
+try:
+    icon = tk.PhotoImage(file=r"Opencv-Projects\Assets\window-icon.png")  # Replace "my_icon.png" with the path to your image file
+    app.iconphoto(False, icon)
+except tk.TclError:
+    print("Error: Could not load icon file.")
+
+app.state('zoomed')
+
+label = tk.Label(app, text = "Hello, User!")
+label.pack()
+label.grid(row=0, column=0)
+
+app.mainloop()
