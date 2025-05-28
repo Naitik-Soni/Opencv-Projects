84868a2d1b9b591b67c572ea972deb748829c21c
Naitik Soni <naitiksoni1705@gmail.com>
2025-05-28 09:05:12 +0530

    Added the code for blurring

--- basic_image_filters.py
+++ basic_image_filters.py
@@ -1,13 +1,12 @@
 import cv2
-import numpy as np
 
 # Load the image
-image_path = r"P:\Computer vision Experiments\Sample images\image4.jpg"
+image_path = r"P:\Computer vision Experiments\Sample images\image6.jpg"
 image = cv2.imread(image_path)
 
 # Resizing the image for fitting into the window
 h, w = image.shape[:2]
-resize_fac = 0.5
+resize_fac = 0.7
 image = cv2.resize(image, (int(w*resize_fac), int(h*resize_fac)))
 
 # Convert to grayscale
@@ -16,7 +15,22 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # -----------------------------------------------------------------------
 # All other operations goes here
 
+avging = cv2.blur(image,(10,10))
+ 
+cv2.imshow('Averaging',avging)
 
+# Gaussian Blurring
+# Again, you can change the kernel size
+gausBlur = cv2.GaussianBlur(image, (5,5),0) 
+cv2.imshow('Gaussian Blurring', gausBlur)
+
+# Median blurring
+medBlur = cv2.medianBlur(image,5)
+cv2.imshow('Media Blurring', medBlur)
+
+# Bilateral Filtering
+bilFilter = cv2.bilateralFilter(image,9,75,75)
+cv2.imshow('Bilateral Filtering', bilFilter)
 
 # ----------------------------------------------------------------------
 
