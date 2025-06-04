efe8b954c3a4655d3386dcb5c4e57f09165ac6ea
Naitik Soni <naitiksoni1705@gmail.com>
2025-06-04 22:16:56 +0530

    Converted tasklist to md

--- edge_detection.py
+++ edge_detection.py
@@ -3,12 +3,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Load the image
-image_path = r"P:\Computer vision Experiments\Sample images\cube.jpg"
+image_path = r"P:\Computer vision Experiments\Sample images\bed.jpg"
 image = cv2.imread(image_path)
 
 # Resizing the image for fitting into the window
 h, w = image.shape[:2]
-resize_fac = 0.7
+resize_fac = 0.3
 image = cv2.resize(image, (int(w*resize_fac), int(h*resize_fac)))
 
 # Convert to grayscale
@@ -16,11 +16,17 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # ----------------------------------------------------------------------
 
-sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3)
-sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)
-sobel = cv2.magnitude(sobelx, sobely)
+canny1 = cv2.Canny(image, 0, 64)
+cv2.imshow("Canny 1", canny1)
 
-cv2.imshow("Sobel", sobel)
+canny2 = cv2.Canny(image, 64, 128)
+cv2.imshow("Canny 2", canny2)
+
+canny3 = cv2.Canny(image, 128, 192)
+cv2.imshow("Canny 3", canny3)
+
+canny4 = cv2.Canny(image, 192, 255)
+cv2.imshow("Canny 4", canny4)
 
 # ----------------------------------------------------------------------
 
