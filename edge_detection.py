552d9ad5451d759e3227d3c334e987f65777e302
Naitik Soni <naitiksoni1705@gmail.com>
2025-06-07 13:50:35 +0530

    Added files

--- edge_detection.py
+++ edge_detection.py
@@ -3,12 +3,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Load the image
-image_path = r"P:\Computer vision Experiments\Sample images\bed.jpg"
+image_path = r"P:\Computer vision Experiments\Sample images\image6.jpg"
 image = cv2.imread(image_path)
 
 # Resizing the image for fitting into the window
 h, w = image.shape[:2]
-resize_fac = 0.3
+resize_fac = 0.6
 image = cv2.resize(image, (int(w*resize_fac), int(h*resize_fac)))
 
 # Convert to grayscale
